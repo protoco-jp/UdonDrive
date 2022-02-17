bc50f048b19bdb359544f25b10cf26015638a15b
protoco-jp <protoco.jp@gmail.com>
2022-02-17 22:08:48 +0900

    Added rigidbody testing script

---
+++ Assets/UdonDrive/Test/TakeOwner.cs
@@ -0,0 +1,12 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TakeOwner : UdonSharpBehaviour {
+    [SerializeField] GameObject gameObj;
+    public override void Interact() {
+        Networking.SetOwner(Networking.LocalPlayer, gameObj);
+    }
+}
