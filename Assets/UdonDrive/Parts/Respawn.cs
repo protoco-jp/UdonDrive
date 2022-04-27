4f4e0991171469816570076567f4bbfe08ad4c5c
protoco-jp <protoco.jp@gmail.com>
2022-04-27 20:27:48 +0900

    リスポーン実装

---
+++ Assets/UdonDrive/Parts/Respawn.cs
@@ -0,0 +1,32 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+namespace UdonDrive {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class Respawn : UdonSharpBehaviour {
+        [SerializeField] float refreshRange = 50f;
+        [SerializeField] Transform[] respawnPoints;
+        [SerializeField] GameObject physicalBody;
+        [SerializeField] Rigidbody rigid;
+        private Transform currentRespawn;
+        public override void Interact() {
+            if (!Networking.IsOwner(physicalBody)) { return; }
+            rigid.velocity = Vector3.zero;
+            rigid.transform.position = currentRespawn.position;
+            rigid.transform.rotation = currentRespawn.rotation;
+        }
+        void Start() {
+            currentRespawn = respawnPoints[0];
+        }
+        void Update() {
+            foreach (Transform point in respawnPoints) {
+                if (Vector3.Distance(this.transform.position, point.position) < refreshRange) {
+                    currentRespawn = point;
+                    break;
+                }
+            }
+        }
+    }
+}
