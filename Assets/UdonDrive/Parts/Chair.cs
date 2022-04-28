2f34ccd818d41dcf3cb588eb2ba44a7d6bdeb738
protoco-jp <protoco.jp@gmail.com>
2022-04-29 04:27:01 +0900

    chair

---
+++ Assets/UdonDrive/Parts/Chair.cs
@@ -0,0 +1,50 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common;
+
+namespace UdonDrive {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class Chair : UdonSharpBehaviour {
+        [SerializeField] Transform entrypoint;
+
+        [UdonSynced, FieldChangeCallback(nameof(height))]
+        private float _height;
+        public float height {
+            set {
+                Debug.Log("toggling the object...");
+                _height = value;
+                entrypoint.localPosition = new Vector3(0, value, 0);
+            }
+            get => _height;
+        }
+
+        private VRCStation vrcStation;
+
+        void Start() {
+            vrcStation = (VRCStation)GetComponent(typeof(VRCStation));
+        }
+        public override void Interact() {
+            Networking.LocalPlayer.UseAttachedStation();
+        }
+        public override void OnStationEntered(VRCPlayerApi player) {
+            if (!player.isLocal) { return; }
+            SendCustomEventDelayedSeconds("enter", 2f);
+        }
+        public override void InputJump(bool value, UdonInputEventArgs args) {
+            vrcStation.ExitStation(Networking.LocalPlayer);
+        }
+        public override void OnStationExited(VRCPlayerApi player) {
+            if (!player.isLocal) { return; }
+            Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
+            height = 0f;
+        }
+        public void enter() {
+            Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
+            height = this.transform.position.y + 0.05f //5cm浮かせる
+                - Networking.LocalPlayer.GetBonePosition(HumanBodyBones.LeftUpperLeg).y;
+        }
+    }
+}
--- Assets/UdonDrive/Parts/DriverChair.cs
+++ Assets/UdonDrive/Parts/DriverChair.cs
@@ -3,22 +3,40 @@ using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using VRC.Udon.Common;
 
 namespace UdonDrive {
     [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
     public class DriverChair : UdonSharpBehaviour {
+        [UdonSynced, FieldChangeCallback(nameof(height))]
+        private float _height;
+        public float height {
+            set {
+                Debug.Log("toggling the object...");
+                _height = value;
+                entrypoint.localPosition = new Vector3(0, value, 0);
+            }
+            get => _height;
+        }
+
         [SerializeField] UpdateCore updateCore;
         [SerializeField] GameObject physicalBody;
         [SerializeField] GameObject networkEngine;
         [SerializeField] GameObject networkWheelL;
         [SerializeField] GameObject networkWheelR;
         [SerializeField] Animator audioAnim;
-
+        [SerializeField] Transform entrypoint;
         [SerializeField] SideBrake sidebrake;
+
+        [SerializeField] GameObject leftGrip;
+        [SerializeField] GameObject rightGrip;
 
         private bool someoneDriving = false;
         private bool ownable = false;
-
+        private VRCStation vrcStation;
+        void Start() {
+            vrcStation = (VRCStation)GetComponent(typeof(VRCStation));
+        }
         public override void OnPlayerJoined(VRCPlayerApi player) {
             SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.Owner, "askOwner");
             if (!player.isLocal) { return; }
@@ -46,6 +64,7 @@ namespace UdonDrive {
 
             ownable = true;
             SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "changeDriver");
+            SendCustomEventDelayedSeconds("enter", 2f);
         }
         public void changeDriver() {
             audioAnim.Play("StartUp.StartUp", 1, 0f);
@@ -55,21 +74,36 @@ namespace UdonDrive {
             if (ownable) {
                 sidebrake.nosidebrake();
                 updateCore.setDriver(true);
+                leftGrip.SetActive(true);
+                rightGrip.SetActive(true);
                 ownable = false;
             } else {
                 updateCore.setDriver(false);
+                leftGrip.SetActive(false);
+                rightGrip.SetActive(false);
             }
+        }
+        public override void InputJump(bool value, UdonInputEventArgs args) {
+            vrcStation.ExitStation(Networking.LocalPlayer);
         }
         public override void OnStationExited(VRCPlayerApi player) {
             SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "quitDriver");
             if (player.isLocal) {
                 sidebrake.sidebrake();
+                Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
+                height = 0f;
             }
         }
         public void quitDriver() {
             audioAnim.SetBool("HasDriver", false);
             someoneDriving = false;
             updateCore.setSideBrake(true);
+        }
+
+        public void enter() {
+            Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
+            height = this.transform.position.y + 0.05f //5cm浮かせる
+                - Networking.LocalPlayer.GetBonePosition(HumanBodyBones.LeftUpperLeg).y;
         }
     }
 }
